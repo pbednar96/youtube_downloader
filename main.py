e19de692cb2ef51e9b6b7c0f35a66c3df451e2bb
Petr Bednar <petan.bednar@gmail.com>
2024-06-15 21:49:41 +0200

    Update and format code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
+
 from pytube import YouTube
-import os
 
 
 def download_video():
@@ -38,31 +39,24 @@ def browse_folder():
         folder_path.set(folder_selected)
 
 
-# Create the main window
 root = tk.Tk()
 root.title("YouTube Video/Audio Downloader")
 
-# URL Entry
 tk.Label(root, text="YouTube URL:").grid(row=0, column=0, padx=10, pady=10)
 url_entry = tk.Entry(root, width=50)
 url_entry.grid(row=0, column=1, padx=10, pady=10)
 
-# Folder Path
 folder_path = tk.StringVar()
 tk.Label(root, text="Download Folder:").grid(row=1, column=0, padx=10, pady=10)
 tk.Entry(root, textvariable=folder_path, width=50).grid(row=1, column=1, padx=10, pady=10)
 tk.Button(root, text="Browse", command=browse_folder).grid(row=1, column=2, padx=10, pady=10)
 
-# Download Type
 download_type_var = tk.StringVar(value="video")
 tk.Label(root, text="Download Type:").grid(row=2, column=0, padx=10, pady=10)
 tk.Radiobutton(root, text="Video", variable=download_type_var, value="video").grid(row=2, column=1, sticky="w", padx=10,
                                                                                    pady=10)
 tk.Radiobutton(root, text="Audio", variable=download_type_var, value="audio").grid(row=2, column=1, sticky="e", padx=10,
                                                                                    pady=10)
-
-# Download Button
 tk.Button(root, text="Download", command=download_video).grid(row=3, column=1, padx=10, pady=10)
 
-# Run the application
 root.mainloop()
